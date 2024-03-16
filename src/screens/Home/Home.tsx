4d4d8b9f7b8a1d2b0aeba01c9c5ed9168f8f6131
Tommzydanz <daniela.stomiwa@yahoo.com>
2024-03-16 06:10:28 +0100

    fix(home): update home screen to fetchProducts and display in flatlist

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,13 +1,92 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import firestore from '@react-native-firebase/firestore';
+import {HomeProps} from './interface';
+import ProductItem from '../../components/ProductItem/ProductItem';
+import {IProduct} from '../../provider/ProductProvider/interface';
 
-const Home = function Home() {
+const Home: HomeProps = function Home() {
+  const [products, setProducts] = useState<IProduct[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const fetchProducts = async () => {
+    try {
+      const list: any = [];
+
+      await firestore()
+        .collection('products')
+        .orderBy('uploadTime', 'desc')
+        .get()
+        .then(querySnapshot => {
+          // console.log('Total Posts: ', querySnapshot.size);
+
+          querySnapshot.forEach(prod => {
+            const {product, price, productImg} = prod.data();
+            list.push({
+              id: prod.id,
+              product,
+              price,
+              productImg,
+            });
+          });
+        });
+
+      setProducts(list);
+
+      if (isLoading) {
+        setIsLoading(false);
+      }
+
+      console.log('Classic Products: ', list);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <ActivityIndicator size={'large'} color="black" />
+      </View>
+    );
+  }
+
+  if (!products || products.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No product added yet - start adding some
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Popular</Text>
-      <View>
-        <Text>Home</Text>
-      </View>
+      <FlatList
+        data={products}
+        renderItem={({item}) => (
+          <ProductItem
+            productName={item.product}
+            price={item.price}
+            image={{uri: item.productImg}}
+          />
+        )}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
@@ -23,5 +102,14 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: 'black',
     fontWeight: 'bold',
+  },
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fallbackText: {
+    fontSize: 16,
+    color: 'black',
   },
 });
