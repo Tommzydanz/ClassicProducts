e88eeab4cfb96ccfc0e41a23fa2fb05852b1d284
Tommzydanz <daniela.stomiwa@yahoo.com>
2024-03-16 06:11:55 +0100

    fix(notification.tsx): retrieve notification to display in notification screen as a list

--- src/screens/Notification/Notification.tsx
+++ src/screens/Notification/Notification.tsx
@@ -1,10 +1,34 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {NotificationProps} from './interface';
 
-const Notification = function Notification() {
+const Notification: NotificationProps = function Notification() {
+  const [notifications, setNotifications] = useState<any[]>();
+
+  useEffect(() => {
+    // Retrieve notifications from AsyncStorage on component mount
+    const getNotifications = async () => {
+      const storedNotifications = await AsyncStorage.getItem('notifications');
+      if (storedNotifications) {
+        setNotifications(JSON.parse(storedNotifications));
+      }
+    };
+    getNotifications();
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
-      <Text>Notification</Text>
+      <FlatList
+        data={notifications}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <View style={styles.notificationContainer}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.body}>{item.body}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -15,5 +39,24 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     alignContent: 'center',
+  },
+  title: {
+    fontSize: 16,
+    color: 'black',
+    textAlign: 'justify',
+  },
+  body: {
+    fontSize: 12,
+    color: 'black',
+    textAlign: 'justify',
+  },
+  notificationContainer: {
+    backgroundColor: '#d2d1d1',
+    borderRadius: 4,
+    marginHorizontal: 16,
+    marginVertical: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 4,
   },
 });
